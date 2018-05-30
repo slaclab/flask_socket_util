48373a63d0740d224cb26323567ee332c7a468e6
Murali Shankar <mshankar@slac.stanford.edu>
2018-05-30 14:47:35 -0700

    Syntax error in dependencies.
    Almost there.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flask_socket_util',
-      version='0.0.7',
+      version='0.0.8',
       description='A small utility for using websockets from within Flask for PSDM web services',
       url='https://github.com/slaclab/flask_socket_util.git',
       author='Murali Shankar',
@@ -9,5 +9,5 @@ setup(name='flask_socket_util',
       license='MIT',
       packages=['flask_socket_util'],
       package_data={'flask_socket_util': ['websocket_client.js']},
-      install_requires=['flask >=1.0.0', 'eventlet >=0.23.0', 'flask-socketio >=3.0.0']
+      install_requires=['flask >=1.0.0', 'eventlet >=0.23.0', 'flask-socketio >=3.0.0'],
       zip_safe=False)
