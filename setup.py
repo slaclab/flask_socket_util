1d8d4dbffbafe51b7b411ff25b08167bbd8d0074
Murali Shankar <mshankar@slac.stanford.edu>
2018-05-30 14:40:24 -0700

    Update tag

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flask_socket_util',
-      version='0.0.5',
+      version='0.0.6',
       description='A small utility for using websockets from within Flask for PSDM web services',
       url='https://github.com/slaclab/flask_socket_util.git',
       author='Murali Shankar',
